b34791f88775b088379f64700758cf797d1f80e5
Skye Russ <picklep@clubruss.org>
2026-03-09 09:14:27 -0700

    #5: Small updates to routes.py

--- src/app/api/v1/routes.py
+++ src/app/api/v1/routes.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
 
-from app.routes import category, transaction, user
+from app.routes import reservation, room, user
 
 api_router = APIRouter(prefix="/api/v1")
 
-api_router.include_router(category.api_router)
-api_router.include_router(transaction.api_router)
+api_router.include_router(room.api_router)
+api_router.include_router(reservation.api_router)
 api_router.include_router(user.api_router)
